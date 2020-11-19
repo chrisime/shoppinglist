81a4c7c906e4da9055291d7400f2bc913314af35
Christian Meyer <christian.meyer@gmail.com>
2020-11-19 20:04:43 +0100

    mount grocery item component

--- shopping-list-ui/src/app/app.module.ts
+++ shopping-list-ui/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule }        from './app-routing.module';
+import { AppComponent }            from './app.component';
+import { GroceryTableComponent }   from './grocery-table/grocery-table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule }          from '@angular/material/table';
+import { MatPaginatorModule }      from '@angular/material/paginator';
+import { MatSortModule }           from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
