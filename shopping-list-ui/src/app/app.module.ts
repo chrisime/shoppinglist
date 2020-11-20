2c966427ea82037bc0474ba41291c17319bd9af2
Christian Meyer <christian.meyer@gmail.com>
2020-11-20 12:21:46 +0100

    add modules

--- shopping-list-ui/src/app/app.module.ts
+++ shopping-list-ui/src/app/app.module.ts
@@ -8,19 +8,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule }          from '@angular/material/table';
 import { MatPaginatorModule }      from '@angular/material/paginator';
 import { MatSortModule }           from '@angular/material/sort';
+import { MatDialogModule }         from '@angular/material/dialog';
+import { MatInputModule }          from '@angular/material/input';
+import { MatButtonModule }         from '@angular/material/button';
+import { MatFormFieldModule }      from '@angular/material/form-field';
+import { FormsModule }             from '@angular/forms';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GroceryTableComponent
+    GroceryTableComponent,
+    DialogBoxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
